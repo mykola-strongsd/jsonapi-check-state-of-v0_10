1542db8219355a82b250f46c6d9c1e57c9a57aa9
Mykola <mlyashenko@strongsd.com>
2020-05-19 12:06:40 +0300

    add example polymorphic (TaggableResource)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   belongs_to :taggable, polymorphic: true
+
+  # for test
+  # belongs_to :article, -> { where( tags: { taggable_type: 'Article' } ).eager_load( :tags ) }, foreign_key: 'taggable_id'
+  # belongs_to :comment, -> { where( tags: { taggable_type: 'Comment' } ).eager_load( :tags ) }, foreign_key: 'taggable_id'
 end
   
--- app/resources/api/v1/tag_resource.rb
+++ app/resources/api/v1/tag_resource.rb
@@ -3,7 +3,11 @@ module Api
     class TagResource < BaseResource
       attributes :tag_name
 
-      belongs_to :taggable, polymorphic: true
+      belongs_to :taggable, polymorphic: true, always_include_linkage_data: true
+
+      # for test
+      # belongs_to :article, class_name: 'Article', foreign_key: 'taggable_id'
+      # belongs_to :comment, class_name: 'Comment', foreign_key: 'taggable_id'
     end
   end
 end
