ddc82bf1915ee39912a9a5bd6c1d763226a7c588
Mykola <mlyashenko@strongsd.com>
2020-05-19 18:36:20 +0300

    spec POST /api/comments

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::CommentsController, type: :request do
+RSpec.describe Api::V1::CommentsController, type: :request do  
   before { set_current_user 3 }
+  let(:current_user_id) { 3.to_s }
+  let(:author_id) { 1 }
 
+  # comments#index
   describe 'GET #index' do
     it 'return all items without pagination' do
       get_json api_comments_path
@@ -10,4 +13,55 @@ RSpec.describe Api::V1::CommentsController, type: :request do
       expect(parsed_json[:meta][:"record-count"]).to eq 45
     end
   end
+
+  # comments#create
+  describe 'POST /api/comments' do
+    let(:payload) do
+      { "data": { "type": "comments", 
+        "relationships": {
+            "article": { "data": { "id": "1", "type": "articles" } },
+            "author": { "data": { "type": "authors", "id": author_id } }
+        },
+        "attributes": { "content": "comment new" }
+      } }
+    end
+
+    it 'works' do
+      header "Content-Type", 'application/vnd.api+json'
+      post api_comments_path, payload.to_json
+      expect_status 201
+    end
+  end  
+
+  # comments#show
+  describe 'GET /api/comments/:id' do
+    it 'works' do
+      #TODO
+      expect_status 200
+    end
+  end  
+
+  # comments#update
+  describe 'PATCH /api/comments/:id' do
+    it 'works' do
+      #TODO
+      expect_status 200
+    end
+  end  
+
+  # comments#update
+  describe 'PUT /api/comments/:id' do
+    it 'works' do
+      #TODO
+      expect_status 200
+    end
+  end  
+
+  # comments#destroy
+  describe 'DELETE /api/comments/:id' do
+    it 'works' do
+      #TODO
+      expect_status 200
+    end
+  end  
 end
